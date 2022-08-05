99755f25c0962ea35636dfaf5bf639e962d9bef6
JumpmanFR <jumpman@jumpman.fr>
2022-08-05 13:18:03 +0200

    Added delay between animation loops

--- js/anim.js
+++ js/anim.js
@@ -3,33 +3,34 @@ JumpmanFR 2021-2022
 Animation method taken from Michael Romanov https://www.sitepoint.com/frame-by-frame-animation-css-javascript/
 Animations by Sam the Salmon */
 
-
 const ANIM_FOLDER = "anim";
-const ANIM_TIME_PER_FRAME = 40;
+const ANIM_TIME_PER_FRAME = 50;
+const ANIM_TIME_BETWEEN_LOOPS = 4000;
 var NB_FRAMES_PER_GAME = {[ID_MOTHER_1]: 192, [ID_MOTHER_2]: 139, [ID_MOTHER_3]: 410};
 
 var gGameCard;
 var gCurrentGameIndex;
 
+var gLoopTimeout;
 
 function startOneAnim() {
+	clearTimeout(gLoopTimeout);
 	var gameId = GAMES_LIST[gGameCard] ? GAMES_LIST[gGameCard].included[gCurrentGameIndex] : "";
 	if (gameId) {
-		el(ELT_ANIMATION).onanimationiteration = onAnimLoop;
-		var nbFrames = NB_FRAMES_PER_GAME[gameId];
-		var animDuration = ANIM_TIME_PER_FRAME * nbFrames / 1000;
-
 		// To reset the animation:
 		el(ELT_ANIMATION).style.animation = 'none';
 		el(ELT_ANIMATION).offsetHeight; // DOM reflow
 		el(ELT_ANIMATION).style.animation = null;
 
+		var nbFrames = NB_FRAMES_PER_GAME[gameId];
+		var animDuration = ANIM_TIME_PER_FRAME * nbFrames / 1000;
 		el(ELT_ANIMATION).style.backgroundImage = `url('${ANIM_FOLDER}/${gameId}.png')`;
 		el(ELT_ANIMATION).style.animationDuration = `${animDuration}s`;
 		el(ELT_ANIMATION).style.animationTimingFunction = `steps(${nbFrames - 1})`;
+		el(ELT_ANIMATION).onanimationend = onAnimLoop;
 	} else {
 		el(ELT_ANIMATION).style.backgroundImage = "none";
-		el(ELT_ANIMATION).onanimationiteration = function() {};
+		el(ELT_ANIMATION).onanimationend = function() {};
 	}
 }
 
@@ -40,8 +41,8 @@ function setGameAnim(gameCard) {
 }
 
 function onAnimLoop(e) {
-	if (GAMES_LIST[gGameCard].included.length > 1) {
+	gLoopTimeout = setTimeout(function() {
 		gCurrentGameIndex = (gCurrentGameIndex + 1) % GAMES_LIST[gGameCard].included.length;
 		startOneAnim();
-	}
+	}, ANIM_TIME_BETWEEN_LOOPS);
 }
