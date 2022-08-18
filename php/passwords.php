cb46a50c29b7d4e287328079ba4063dce3848823
JumpmanFR <jumpman@jumpman.fr>
2022-08-18 12:12:06 +0200

    Added (abstract) passwords.php

---
+++ php/passwords.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username=""; // To user name to access the database
+$password=""; // The password to access the database
+$database=""; // The name of the database
+$table=""; // The table that contains the usage counter
+?>
