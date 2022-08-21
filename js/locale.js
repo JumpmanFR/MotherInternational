d72a66f2e1324d04e088203b480b6af14f2b43c0
JumpmanFR <jumpman@jumpman.fr>
2022-08-21 19:21:17 +0200

    Added comments in locale.js to help translators

--- js/locale.js
+++ js/locale.js
@@ -1,49 +1,49 @@
 const LOCALIZATION = {
 	'en':
 	{
-		'txtHtmlTitle':				'Mother International',
-		'txtTitleBottom':			'International',
+		'txtHtmlTitle':				'Mother International', /* 'Mother' should stay 'Mother', but 'International' can be translated */
+		'txtTitleBottom':			'International', /* same here, but this one is for the logo */
 		'txtSubtitle':				'All-in-one MOTHER Translation Patcher',
 
-		'txtBrowse':				'Browse…',
-		'txtNoRom':					'',
+		'txtBrowse':				'Browse…', /* a classic button to let the user browse their files */
+		'txtNoRom':					'', /* we decided to write nothing here, after all */
 
 		'txtSpecifyRom':			'Please drop any MOTHER game ROM',
-		'txtApplyPatch':			'Apply Translation',
-		'txtUpdate':				'Update',
+		'txtApplyPatch':			'Apply Translation', /* could be 'Apply Patch' as well */
+		'txtUpdate':				'Update', /* the button changes to 'Update' instead of 'Apply Patch' when it’s a more recent translation */
 
 		'txtAnalyzingFile':			'Analyzing file…',
 		'txtAnalyzingRom':			'Analyzing ROM…',
 		'txtRomIdentified':			'ROM identified!',
-		'txtDownloading':			'Downloading patch%…',
+		'txtDownloading':			'Downloading patch%…', /* % is a progress indicator, like (1/2) or (50%); can also be an empty string (extra space not needed) */
 		'txtUnzipping':				'Unzipping…',
-		'txtApplyingPatch':			'Applying patch%…',
+		'txtApplyingPatch':			'Applying patch%…', /* % is a progress indicator, like (1/2) or (50%); can also be an empty string (extra space not needed) */
 		'txtZipping':				'Creating zip file…',
 		'txtFinalizing':			'Finalizing…',
 		'txtEndMsg':				'Your patched ROM is ready!',
 
-		'txtChecksum':				'CRC-32 checksum: %',
+		'txtChecksum':				'CRC-32 checksum: %', /* if this text is long, % should preferably not be at the very end */
 
-		'txtAllTranslations':		'Available options for %:',
-		'txtNoTranslation':			'No translation available',
-		'txtShowAllVersions':		'Show all versions',
+		'txtAllTranslations':		'Available options for %:', /* % is the name of a game */
+		'txtNoTranslation':			'No translation available', /* in the select menu if empty */
+		'txtShowAllVersions':		'Show all versions', /* this is a checkbox */
 
-		'txtDescVersion':			'v',
-		'txtDescBy':				'by',
-		'txtPatched':				'% patch', /* % is the adjective for the language */
-		'txtUnpatched':				'%, unpatched',
-		'txtDescOfficial':			'official',
-		'txtDescUpdate':			'(update)',
-		'txtDescIncompatible':		'(incompatible)',
-		'txtVisitSite':				'%’s webpage',
-		'txtVisitSiteAt':			'at %',
-		'txtUpdateInfo':			'Version info',
-		'txtReadDoc':				'Readme file',
+		'txtDescVersion':			'v', /* just before a version number */
+		'txtDescBy':				'by', /* just before the name of a translation team or individual */
+		'txtPatched':				'% patch', /* % is the adjective for the language; this string will appear of a file name */
+		'txtUnpatched':				'%, unpatched', /* % is the adjective for the language; this string will appear of a file name */
+		'txtDescOfficial':			'official', /* extra indications in the select menu for certain translation choices */
+		'txtDescUpdate':			'(update)', /* extra indications in the select menu for certain translation choices */
+		'txtDescIncompatible':		'(incompatible)', /* extra indications in the select menu for certain translation choices */
+		'txtVisitSite':				'%’s webpage', /* % is the name of a translation team or individual */
+		'txtVisitSiteAt':			'at %', /* % is the base name of a URL */
+		'txtUpdateInfo':			'Version info', /* the text of a link to get more info on a specific version */
+		'txtReadDoc':				'Readme file', /* the text of a link to download a readme file */
 
-		'txtNbUses':				'Number of uses for this patch: %',
-		'txtNbUsesUnknown':			'unknown',
+		'txtNbUses':				'Number of uses for this patch: %', /* if this text is long, % should preferably not be at the very end */
+		'txtNbUsesUnknown':			'unknown', /* replaces the % if the server did not return the number of uses */
 
-		'txtReadmeFile':			'readme',
+		'txtReadmeFile':			'readme', /* will appear in a file name */
 
 		'error_no_rom':				'No ROM specified',
 		'error_no_rom_info':		'This ROM hasn’t been analyzed',
@@ -61,7 +61,7 @@ const LOCALIZATION = {
 		'warning_too_big':			'Too big files',
 		'Not Found':				'A patch file was not found',
 
-		'txtAbout':					'Credits & About',
+		'txtAbout':					'Credits & About', /* text for the credits button; could be just 'Credits' or 'About' as well */
 
 		'txtCredits':				'Credits',
 		'txtCreditsDesign':			'Design',
@@ -74,17 +74,17 @@ const LOCALIZATION = {
 		'txtAboutIntro':			'Mother International is an online patching application that can translate any game in the MOTHER series.',
 		'txtAboutFreeProg1':		'Mother International is a free to use program.',
 		'txtAboutFreeProg2':		'This program is not for sale.',
-		'txtAboutSource':			'The source code of Mother International is % under the ‰.',
-		'txtAboutSourceGitHub':		'distributed',
-		'txtAboutSourceLicense':	'MIT license',
+		'txtAboutSource':			'The source code of Mother International is % under the ‰.', /* % and ‰ are two clickable links */
+		'txtAboutSourceGitHub':		'distributed', /* the text for the first clickable link I’ve just talked about */
+		'txtAboutSourceLicense':	'MIT license', /* the second one */
 		'txtAboutTeams':			'All rights reserved to the teams who created each translation patch packaged into this program. Special thanks to them! POTENTIAL!',
 		'txtAboutAllTranslations':	'All supported translations:',
-		'txtAboutAllTransLabel':	'– Select to access webpage –',
-		'txtAboutAllTransNoSite':	'No registered webpage for %.',
-		'txtAboutAllTransSiteAsk':	'Do you want to access %’s webpage at ‰?',
-		'txtAboutTranslator1':		'If you’re a translator, feel free to %!',
-		'txtAboutTranslator2':		'contact us',
-		'txtAboutVersion':			'Version %',
+		'txtAboutAllTransLabel':	'– Select to access webpage –', /* the top entry in a list of translations */
+		'txtAboutAllTransNoSite':	'No registered webpage for %.', /* the text in an alert window */
+		'txtAboutAllTransSiteAsk':	'Do you want to access %’s webpage at ‰?', /* the text in an alert window */
+		'txtAboutTranslator1':		'If you’re a translator, feel free to %!', /* % is the text 'contact us' as a clickable link */
+		'txtAboutTranslator2':		'contact us', /* this is what I was just referring to */
+		'txtAboutVersion':			'Version %', /* % is a version number */
 	},
 	'fr':
 	{
