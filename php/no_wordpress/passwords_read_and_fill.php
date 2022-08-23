f5ccdf75e19f2d54dece0eb26a75812175bf65c4
JumpmanFR <jumpman@jumpman.fr>
2022-08-23 17:55:24 +0200

    Default file pattern for passwords.php

--- php/no_wordpress/passwords_read_and_fill.php
+++ php/no_wordpress/passwords_read_and_fill.php
@@ -1,4 +1,5 @@
 <?php
+// Please fill in this file and rename it to passwords.php
 $servername = "localhost";
 $username=""; // To user name to access the database
 $password=""; // The password to access the database
